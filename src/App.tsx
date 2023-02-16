772b056c7204a713c69f897ee275dc4841727273
shivprajapat <shivkumarprajapat232@gmail.com>
2023-02-16 16:53:38 +0530

    prettier setup

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className="bg-gray-20">
+    <div className="app bg-gray-20">
       <h1 className='text-4xl font-bold'>Hello GYM WebSite</h1>
     </div>
   )
